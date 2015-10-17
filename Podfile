ff4b752d497b9882f5d730e415012a436e08df67
George Lo <twgeolo@gmail.com>
2015-10-16 22:04:13 -0400

    added Alamofire

--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
 platform :ios, ‘9.0’
+use_frameworks!
 
 target 'Simplify' do
-	pod 'AFNetworking'
+	pod 'Alamofire’
 end
 
---
+++ Pods/Target Support Files/Pods-Simplify/Pods-Simplify-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SimplifyVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SimplifyVersionString[];
+
